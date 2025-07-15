de0e4513178965c3a9f6181d34c0cab602a0ee39
Blue-Leaf-vm <yellowaholotle@gmail.com>
2025-07-15 19:36:09 +0900

    부팅송 예외 설정

--- skin/2series/code.js
+++ skin/2series/code.js
@@ -469,7 +469,7 @@ async function loading(status=0, file='', cursize=0, filesize=1, stat=null) {
 		bga.play();
 
 		systemsound.addEventListener('ended', function(){
-			loading(4);
+			if (loadingstat==4) loading(4);
 			systemsound.removeEventListener('ended', arguments.callee);
 		});
 	} else {
